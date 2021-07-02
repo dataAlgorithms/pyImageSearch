0759836b0b6a0703270e5cd2cf54ad40c4cf8823
dataAlgorithms <195527444@qq.com>
2021-07-02 20:31:23 +0800

    use tensorflow data to PPL

---
+++ useTfDataPPL/build_dataset.py
@@ -0,0 +1,55 @@
+from pyimagesearch import config
+from imutils import paths
+import random
+import shutil
+import os
+
+# grab the paths to all  input images in the original input directory
+# and shuffle them
+imagePaths = list(paths.list_images(config.ORIG_INPUT_DATASET))
+random.seed(42)
+random.shuffle(imagePaths)
+
+# compute the training and testing split
+i = int(len(imagePaths) * config.TRAIN_SPLIT)
+trainPaths = imagePaths[:i]
+testPaths = imagePaths[i:]
+
+# we'll be using part of the training data for validation
+i = int(len(trainPaths) * config.VAL_SPLIT)
+valPaths = trainPaths[:i]
+trainPaths = trainPaths[i:]
+
+# define the dataset that we'll be building
+datasets = [
+  ("training", trainPaths, config.TRAIN_PATH),
+  ("validation", valPaths, config.VAL_PATH),
+  ("testing", testPaths, config.TEST_PATH)
+]
+
+# loop over the datasets
+for (dType, imagePaths, baseOutput) in datasets:
+    # show which data split we are creating
+    print("[Info] building '{}' split".format(dType))
+
+    # if the output base output directory does not exist, create it
+    if not os.path.exists(baseOutput):
+        print("[Info] 'creating {}' directory".format(baseOutput))
+        os.makedirs(baseOutput)
+
+    # loop over the input image paths
+    for inputPath in imagePaths:
+        filename = inputPath.split(os.path.sep)[-1]
+        label = filename[-5:-4]
+
+        # build the path to the label directory
+        labelPath = os.path.sep.join([baseOutput, label])
+
+        if not os.path.exists(labelPath):
+            print("[Info] 'creating {}' direcotyr".format(labelPath))
+            os.makedirs(labelPath)
+
+        # construct the path to the destination image and then copy
+        # the image itself
+        p = os.path.sep.join([labelPath, filename])
+        shutil.copy2(inputPath, p) 
---
+++ useTfDataPPL/train_model.py
@@ -0,0 +1,109 @@
+import matplotlib
+matplotlib.use("Agg")
+
+from pyimagesearch.cancernet import CancerNet
+from pyimagesearch import config
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import Adagrad
+from tensorflow.keras.utils import to_categorical
+from tensorflow.data.experimental import AUTOTUNE
+from imutils import paths
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import argparse
+import os
+
+# load imges
+def load_images(imagePath):
+    # read the image from disck, decode it, convert the data type to 
+    # floating point, and resize it
+    image = tf.io.read_file(imagePath)
+    image = tf.image.decode_png(image, channels=3)
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    image = tf.image.resize(image, config.IMAGE_SIZE)
+
+    # parse the class label from the file path
+    label = tf.strings.split(imagePath, os.path.sep)[-2]
+    label = tf.strings.to_number(label, tf.int32)
+
+    # return the image and the label
+    return (image, label)
+
+@tf.function
+def augment(image, label):
+    # perform random horizontal and vertical flips
+    image = tf.image.random_flip_up_down(image)
+    image = tf.image.random_flip_left_right(image)
+    
+    # return the image and label
+    return (image, label)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+     help="path to output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+# grab all the train/validation/test
+trainPaths = list(paths.list_images(config.TRAIN_PATH))
+valPaths = list(paths.list_images(config.VAL_PATH))
+testPaths = list(paths.list_images(config.TEST_PATH))
+
+# calculate the total number of training images in each class and 
+# initalize a dictionary to store the class weights
+trainLabels = [int(p.split(os.path.sep)[3]) for p in trainPaths]
+trainLabels = to_categorical(trainLabels)
+classTotals = trainLabels.sum(axis=0)
+classWeight = {}
+
+# loop over all classes and calculte the class weight
+for i in range(0, len(classTotals)):
+    classWeight[i] = classTotals.max() /classTotals[i]
+
+# build the training dataset and data iput pipeline
+trainDS = tf.data.Dataset.from_tensor_slices(trainPaths)
+trainDS = (trainDS.shuffle(len(trainPaths)).map(load_images, num_parallel_calls=AUTOTUNE).map(augment, num_parallel_calls=AUTOTUNE).cache().batch(config.BS).prefetch(AUTOTUNE))
+
+# build the validation dataset and data input pipeline
+valDS = tf.data.Dataset.from_tensor_slices(valPaths)
+valDS = (valDS.map(load_images, num_parallel_calls=AUTOTUNE).cache().batch(config.BS).prefetch(AUTOTUNE))
+
+# build the testing dataset and data input pipeline
+testDS = tf.data.Dataset.from_tensor_slices(testPaths)
+testDs = (testDS.map(load_images, num_parallel_calls=AUTOTUNE).cache().batch(config.BS).prefetch(AUTOTUNE))
+
+# intialize model and compile it
+model = CancerNet.build(width=48, height=48, depth=3, classes=1)
+opt = Adagrad(lr=config.INIT_LR,
+    decay=config.INIT_LR/config.NUM_EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt, 
+        metrics=["accuracy"])
+
+# initialize an early stopping callback to prevent the model from
+# overfitting
+es = EarlyStopping(
+	monitor="val_loss",
+	patience=config.EARLY_STOPPING_PATIENCE,
+	restore_best_weights=True)
+# fit the model
+H = model.fit(
+	x=trainDS,
+	validation_data=valDS,
+	class_weight=classWeight,
+	epochs=config.NUM_EPOCHS,
+	callbacks=[es],
+	verbose=1)
+# evaluate the model on test set
+(_, acc) = model.evaluate(testDS)
+print("[INFO] test accuracy: {:.2f}%...".format(acc * 100))
+
+# plot the training loss and accuracy
+plt.style.use('ggplot')
+plt.figure()
+plt.plot(H.history["loss"], label="train_loss")
+plt.plot(H.history["val_loss"], label='val_loss')
+plt.plot(H.history["accuracy"], label="val_acc")
+plt.title("train loss and acc on dataset")
+plt.xlabel("epoch")
+plt.ylabel("loss/acc")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
